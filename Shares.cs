4b6384ae4eadcf96bf62739d6f9c8065d102bf90
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-26 01:58:48 +0300

    Shares

--- Shares.cs
+++ Shares.cs
@@ -1,43 +1,72 @@
 public class Shares
 {
-    public static int Size (string msg) //Определяем размер массива
-{
-    Console.Write(msg);
-    int n = Convert.ToInt32(Console.ReadLine());
-    return n;
-}
-
-public static int [] Array (int size) //Заполняем массив случайными трехзначными числами
-{
-    int [] array = new int [size];
-    for (int i = 0; i<size; i++)
+    public static int Size(string msg) //Определяем размер массива
     {
-        array [i] = new Random().Next(100, 1000);
-        Console.Write (array[i]+ " ");
+        Console.Write(msg);
+        int n = Convert.ToInt32(Console.ReadLine());
+        return n;
     }
-return array;
-}
 
-public static int [] ArrayRandom (int sizeRandom) //Заполняем массив случайными числами от 1 до 100
-{
-    int [] array = new int [sizeRandom];
-    for (int i = 0; i<sizeRandom; i++)
+    public static int[] Array(int size) //Заполняем массив случайными трехзначными числами
     {
-        array [i] = new Random().Next(1, 100);
-        Console.Write (array[i]+ " ");
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = new Random().Next(100, 1000);
+            Console.Write(array[i] + " ");
+        }
+        return array;
     }
-return array;
-}
 
-public static int col (int [] NewArray) //Определяем количество четных элементов
-{
-    int a = 0;
-    for (int i = 0; i<NewArray.Length; i++)
+    public static int[] ArrayRandom(int sizeRandom) //Заполняем массив случайными числами от 1 до 100
     {
-        if (NewArray[i]%2 == 0) a=a+1;
+        int[] array = new int[sizeRandom];
+        for (int i = 0; i < sizeRandom; i++)
+        {
+            array[i] = new Random().Next(1, 100);
+            Console.Write(array[i] + " ");
+        }
+        return array;
     }
-    return a;
-}
 
+    public static int col(int[] NewArray) //Определяем количество четных элементов
+    {
+        int a = 0;
+        for (int i = 0; i < NewArray.Length; i++)
+        {
+            if (NewArray[i] % 2 == 0) a = a + 1;
+        }
+        return a;
+    }
+
+    public static int sum(int[] NewArray) //Определяем количество четных элементов
+    {
+        int a = 0;
+        for (int i = 1; i < NewArray.Length; i = i + 2)
+        {
+            a = a + NewArray[i];
+        }
+        return a;
+    }
+
+    public static int maxElement(int[] NewArray) //Определяем максимальный элемент
+    {
+        int MaxE = NewArray[0];
+        for (int i = 1; i < NewArray.Length; i++)
+        {
+            if (MaxE < NewArray[i]) MaxE = NewArray[i];
+        }
+        return MaxE;
+    }
+
+    public static int minElement(int[] NewArray) //Определяем минимальный элемент
+    {
+        int MinE = NewArray[0];
+        for (int i = 1; i < NewArray.Length; i++)
+        {
+            if (MinE > NewArray[i]) MinE = NewArray[i];
+        }
+        return MinE;
+    }
 
 }
