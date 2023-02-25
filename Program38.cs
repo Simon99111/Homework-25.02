e6a1e7efa4a7b44263d849384c27cafcad2042f4
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-26 02:00:36 +0300

    Задача 38

---
+++ Program38.cs
@@ -0,0 +1,10 @@
+//Перед проверкой снять комментарии снизу
+
+// using static Shares;
+
+// int n = Size("Введите размер количество элементов массива ");
+// int[] Print = ArrayRandom(n);
+// int max = maxElement(Print);
+// int min = minElement(Print);
+// Console.WriteLine();
+// Console.WriteLine("Разницу между максимальным и минимальным элементов массива = " + (max - min));
