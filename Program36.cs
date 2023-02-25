c9d22350882ae3ec12207d3e6339c24c3cb28688
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-26 01:22:41 +0300

    Программа 36

---
+++ Program36.cs
@@ -0,0 +1,7 @@
+using static Shares;
+
+int n = Size("Введите размер количество элементов массива ");
+int [] Print = ArrayRandom (n);
+int summ = sum(Print);
+Console.WriteLine();
+Console.WriteLine(summ);
